40469eddd95329afd9f9a51dae347c006627323e
samims <sam91v@gmail.com>
2018-12-17 23:59:17 +0530

    shuffle function added without proper random function

--- src/deck.go
+++ src/deck.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"strings"
+	"syscall"
 )
 
 type deck []string
@@ -46,5 +48,22 @@ func (d deck) saveToFile(filename string) error {
 
 func newDeckFromFile(filename string) deck {
 	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// option #1 log the error and return a call to newDeck()
+		// option #2 log the error and quit the programming
+		fmt.Println("Error:", err)
+		//os.Exit(1)
+		syscall.Exit(1)
+	}
+	s := strings.Split(string(bs), ",") //Ace of spades, Two of Spades....
+	return deck(s)
 
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+
+	}
 }
--- src/main.go
+++ src/main.go
@@ -1,7 +1,10 @@
 package main
 
 func main() {
+	//cards := newDeck()
+	//cards.saveToFile("my_cards")
 	cards := newDeck()
-	cards.saveToFile("my_cards")
+	cards.shuffle()
+	cards.print()
 
 }
